b7437c05c1eee1ad94eb858fd18489e2c91db2d5
ilovemhuukrob <78673905+ilovemhuukrob@users.noreply.github.com>
2023-03-15 01:21:59 +0700

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,8 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
   ],
   rules: {
-    'vue/multi-word-component-names': 'off',
+    'vue/multi-word-component-names': 0,
   },
 }
